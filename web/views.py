87fc9bdb9d5c73a7f7433c1e3af43385b6a2a8bb
e <pintxz@163.com>
2020-03-19 16:51:34 +0800

    同步测试

--- web/views.py
+++ web/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
-
+import logging
+logger = logging.getLogger('log')
 # Create your views here.
 
 def testhtml(request):
+    logger.info('info的测试！')
+    logger.error('error的测试！')
+    logger.debug('debug的测试！')
     return render(request, 'test.html')
