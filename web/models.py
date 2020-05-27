94b373c0ca09b69a36f8b80a766158c049febcb8
e <pintxz@163.com>
2020-05-27 17:01:24 +0800

    同步测试

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Elements(models.Model):
+    page = models.CharField(max_length=64)
+    element = models.DateTimeField(auto_now_add=True)
+    by = models.DateTimeField(auto_now_add=True)
+    value = models.DateTimeField(auto_now_add=True)
+    custom = models.DateTimeField(auto_now_add=True)
+    remark = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.book_name
--- web/views.py
+++ web/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-import logging
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+import json, logging
+
 logger = logging.getLogger('log')
+
+
 # Create your views here.
 
+@require_http_methods(["GET"])
 def testhtml(request):
     logger.info('info的测试！')
     logger.error('error的测试！')
     logger.debug('debug的测试！')
     return render(request, 'test.html')
+
+
+@require_http_methods(["GET"])
+def add_book(request):
+    response = {}
+    try:
+        book = Book(book_name=request.GET.get('book_name'))
+        book.save()
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except  Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+    return JsonResponse(response)
