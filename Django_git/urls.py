630ee32f159aa49ba5401316bbbed8f8c2ac602f
e <pintxz@163.com>
2020-05-28 16:39:33 +0800

    同步测试

--- Django_git/urls.py
+++ Django_git/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from Django_git.web import views
+from web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
